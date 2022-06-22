79a740a7409f615db0573dc163541faf5821c8bd
Vinicius Benso <vinibenso@gmail.com>
2022-06-22 16:57:37 -0300

    commit sidebar

--- igniteLab/src/components/Lesson.tsx
+++ igniteLab/src/components/Lesson.tsx
@@ -1,5 +1,57 @@
-export function Lesson() {
+import { CheckCircle, Lock } from "phosphor-react"
+import { isPast, format } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
+interface LessonProps {
+    title: string;
+    slug: string;
+    availableAt: Date;
+    type: "Live" | "Class";
+}
+
+export function Lesson(props: LessonProps) {
+
+    const isLessonAvailable = isPast(props.availableAt);
+    const availableDateFormatted = format(props.availableAt, "EEEE' * 'd' de 'MMMM' * 'k'h'mm' ", {locale: ptBR,})
+
+
     return (
-        <h1>Lesson</h1>
+        <a href="#">
+            <span className="text-gray-300">
+                {availableDateFormatted}
+            </span>
+
+            <div className="rounded border border-gray-500 p-4 mt-2">
+                <header className="flex itens-center justify-between">
+                    {isLessonAvailable ? (
+
+                        <span className="text-s text-blue-500 font-medium flex items-center gap-2" >
+                            <CheckCircle size={20} />
+                            Conteúdo liberado
+                        </span>
+
+                    ) : (
+
+                        <span className="text-s text-orange-500 font-medium flex items-center gap-2" >
+                            <Lock size={20} />
+                            Em breve
+                        </span>
+
+                    )}
+
+
+                    <span className="text-sx rounded py-[0.125rem] px-2 text-white border-green-300 border font-bold" >
+                        {props.type === 'Live' ? 'Ao Vivo' : 'Aula Pratica'}
+                    </span>
+
+                </header>
+
+                <strong className="text-gray-200 mt-5 block">
+                    {props.title}
+                </strong>
+
+
+            </div>
+        </a>
     )
 }
--- igniteLab/tailwind.config.js
+++ igniteLab/tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: 'Roboto, sans-serif',
-      }
+        sans: 'Roboto, sans-serif'
+      },
       colors: {green: {
         300: '#00B37E',
         500: '#00875F',
